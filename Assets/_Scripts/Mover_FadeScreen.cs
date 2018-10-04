afd7580ffcee0cd55dd2144aea9ad0f46b46daea
Matt0076 <mattyoung69.my@gmail.com>
2018-10-05 09:04:00 +1000

    Main scene 5/10/18
    - Testing fade in and out screens
    - Testing audio fade out

---
+++ Assets/_Scripts/Mover_FadeScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover_FadeScreen : MonoBehaviour
+
+{
+    public float speed = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, -speed, 0) * 0.05f * Time.deltaTime);
+    }
+}
