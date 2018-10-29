ee7b1d9d8caf156e48c1daafbb30030b7b4e5dd1
tinny <tinnystudios@gmail.com>
2018-10-29 15:47:19 +1100

    Add outro to end experience

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,10 +7,29 @@ using UnityEngine.Audio;
 public class GameManager : MonoBehaviour
 {
     public AudioMixerGroup MainMixerGroup;
+    public ParticleSystem SpiralPS;
+    public AnimationCurve SpiralCurve;
+    public float TotalGameDuration = 180;
 
     private IEnumerator Start()
     {
-        yield return new WaitForSeconds(180);
+        yield return GameLoop();
+        yield return Outro();
+    }
+
+    private IEnumerator GameLoop()
+    {
+        var SpritalMain = SpiralPS.main;
+
+        for (float i = 0; i < 1.0F; i += Time.deltaTime / TotalGameDuration)
+        {
+            SpritalMain.simulationSpeed = Mathf.Lerp(0.25F, 0.05F, SpiralCurve.Evaluate(i));
+            yield return null;
+        }
+    }
+
+    private IEnumerator Outro()
+    {
         ScreenFader.Instance.FadeToBlack(5);
         this.SetAudioMixerVolume(MainMixerGroup.audioMixer, "Volume", 0, 4);
         yield return ScreenFader.Instance.WaitUntilFadeComplete();
