0488fb5da04fc940e7a87cdf1f0d61128d1802dd
tinny <tinnystudios@gmail.com>
2018-11-20 08:17:24 +1100

    Smoother sound

---
+++ Assets/EnableSortGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class EnableSortGroup : MonoBehaviour
+{
+    public SortingGroup SortingGroup;
+    public float Delay = 2;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(Delay);
+        SortingGroup.enabled = true;
+    }
+}
